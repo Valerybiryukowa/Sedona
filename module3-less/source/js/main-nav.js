b2d4546b4f32fd49f8512b607208ff7bdab136df
valeriabirukova <velerya78@gmail.com>
2023-07-09 15:18:34 +0300

    Добавлены отступы к post-review, success-popup

--- module3-less/source/js/main-nav.js
+++ module3-less/source/js/main-nav.js
@@ -5,12 +5,11 @@
         navMain.classList.remove('main-nav--nojs');
         
         navToggle.addEventListener('click', function() {
-          if (navMain.classList.contains('main-nav--closed')) {
+            if (navMain.classList.contains('main-nav--closed')) {
             navMain.classList.remove('main-nav--closed');
             navMain.classList.add('main-nav--opened');
-          } else {
+            } else {
             navMain.classList.add('main-nav--closed');
             navMain.classList.remove('main-nav--opened');
-          }
+            }
         });
-      
